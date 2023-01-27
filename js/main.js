4a73f85a8e4251aea108f0b1ea4fe945e540565d
Seonghee Yun <104363065+intacthee@users.noreply.github.com>
2023-01-27 17:19:44 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,37 @@
+$(function () {
+
+    $('.mainVisual .main_slide').slick();
+
+    $('.mainContent .slide').slick({
+        slidesToShow: 4,
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
